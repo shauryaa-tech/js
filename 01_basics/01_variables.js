21275ca9d14bc83ba58a5e0ed25f555d198e5c8d
Shaurya Pratap Singh <shaurya13822@gmail.com>
2025-01-22 06:38:24 +0000

    container config Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shaurya@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 32    not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity ])
